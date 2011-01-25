5a4bdd2ec3d1ef3a578cee674f813bf894d052bf
selharti <selharti@gmail.com>
2011-01-25 16:39:06 -0500

    Preliminary push (with bugs and errors)

---
+++ app/controllers/training_controller.rb
@@ -0,0 +1,5 @@
+class TrainingController < ApplicationController
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 ActionController::Routing::Routes.draw do |map|
   # The priority is based upon order of creation: first created -> highest priority.
 
+
+  map.resources :service
+  map.resources :company
+  map.resources :contact
+  map.resources :coaching  
+  map.resources :training
+  map.resources :development
+  
+  
+  
+  
   # Sample of regular route:
   #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
   # Keep in mind you can assign values other than :controller and :action
@@ -31,7 +42,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+   map.root :controller => "home"
 
   # See how all your routes lay out with "rake routes"
 
